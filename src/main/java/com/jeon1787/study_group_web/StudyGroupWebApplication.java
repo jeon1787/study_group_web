998ef5b82cf72b52dc1f165337e38301e1fdd192
jeon1787 <wjsdmsdn1787@naver.com>
2022-05-14 01:32:17 +0900

    spring boot 프로젝트 생성

---
+++ src/main/java/com/jeon1787/study_group_web/StudyGroupWebApplication.java
@@ -0,0 +1,13 @@
+package com.jeon1787.study_group_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGroupWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeon1787/study_group_web/StudyGroupWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeon1787.study_group_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGroupWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
